504cd68f9a851ad9db546980278e46513ff255df
Nguyễn Quốc Châu <84402319+nguyenquocchauit@users.noreply.github.com>
2021-10-11 13:26:31 +0700

    Add files via upload

---
+++ Contact/js/contact.js
@@ -0,0 +1,12 @@
+function FB1(){
+    window.open("https://www.facebook.com/NguyenQuocChau.NhaTrang");
+}
+function FB2(){
+    window.open("https://www.facebook.com/NguyenQuocChauIt");
+}
+function Zalo(){
+    window.open("https://zalo.me/0926383006");
+}
+function Instagram(){
+    window.open("https://www.instagram.com/qnocchoune");
+}
